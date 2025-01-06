be25d2b53b282ea7334d05be08492bb772a14d16
vctrfrncsc <66354086+slicklab@users.noreply.github.com>
2025-01-06 17:43:57 +0800

    Delete collision.js

--- collision.js
+++
@@ -1,7 +0,0 @@
-export function checkCollision(player1, player2) {
-    const dx = player2.x - player1.x;
-    const dy = player2.y - player1.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    
-    return distance < player1.radius + player2.radius;
-}
